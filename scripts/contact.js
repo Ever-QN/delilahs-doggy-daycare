c09bf4955839c0b7725aace7c89e68fea2b58083
Quynh Ngo <QuynhTNgo@live.com>
2022-12-10 02:09:07 -0700

    Header, footing, button color fixes

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,10 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
+let submitBtn = document.getElementById("submit-button");
+
+submitBtn.addEventListener("click", function submitMessage() {
+    
+})
+
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
